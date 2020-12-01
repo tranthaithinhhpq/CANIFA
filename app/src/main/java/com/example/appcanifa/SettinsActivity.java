3f21636f11293522149f47a1cc23d2d5454b9b89
tranthaithinhhpq <55739883+tranthaithinhhpq@users.noreply.github.com>
2020-12-02 00:02:10 +0700

    commit dau tien

---
+++ app/src/main/java/com/example/appcanifa/SettinsActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcanifa;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settins);
+    }
+}
