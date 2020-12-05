ebc1476770e0786cc55becadc5ce3f2c588f9c11
tranthaithinhhpq <55739883+tranthaithinhhpq@users.noreply.github.com>
2020-12-05 11:28:25 +0700

    24

--- app/src/main/java/com/example/appcanifa/HomeActivity.java
+++ app/src/main/java/com/example/appcanifa/HomeActivity.java
@@ -62,8 +62,8 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -181,7 +181,8 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.nav_cart)
         {
-
+            Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+            startActivity(intent);
         }
         else if (id == R.id.nav_order)
         {
