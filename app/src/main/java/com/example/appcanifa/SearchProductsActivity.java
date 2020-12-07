6f9d86186b5f9fb15146a98d7bfc586e402a6c07
thinh202 <55739822+thinh202@users.noreply.github.com>
2020-12-07 09:40:54 +0700

    sua loi

--- app/src/main/java/com/example/appcanifa/SearchProductsActivity.java
+++ app/src/main/java/com/example/appcanifa/SearchProductsActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -18,10 +20,11 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class SearchProductsActivity extends AppCompatActivity {
     private Button SearchBtn;
-    private String Search_Input;
+    private String SearchInput;
     private EditText inputText;
     private RecyclerView searchList;
 
@@ -37,33 +40,53 @@ public class SearchProductsActivity extends AppCompatActivity {
         SearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Search_Input = inputText.getText().toString();
+                SearchInput = inputText.getText().toString();
                 onStart();
             }
         });
     }
 
 
-
     protected void OnStart() {
         super.onStart();
+    }
+
+
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
-        FirebaseRecyclerOptions<Products>options=
-                 new FirebaseRecyclerOptions.Builder<Products>().setQuery(reference.orderByChild("pname"),Products.class)
+
+         FirebaseRecyclerOptions<Products>options=
+
+                  new FirebaseRecyclerOptions.Builder<Products>().setQuery(reference.orderByChild("pname").startAt(SearchInput),Products.class)
                 .build();
-        FirebaseRecyclerAdapter<Products , ProductViewHolder> adapter =
+                final FirebaseRecyclerAdapter<Products , ProductViewHolder> adapter =
                  new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
                      @Override
-                     protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
+                     protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model)
+                      {
+                          holder.txtProductName.setText(model.getPname());
+                          holder.txtProductDescription.setText(model.getDescription());
+                          holder.txtProductPrice.setText("Price = " + model.getPrice() + "$");
+                          Picasso.get().load(model.getImage()).into(holder.imageView);
 
-                     }
-
+                          holder.itemView.setOnClickListener(new View.OnClickListener() {
+                              @Override
+                              public void onClick(View view) {
+                                  Intent intent = new Intent(SearchProductsActivity.this, ProductDetailsActivity.class);
+                                  intent.putExtra("pid", model.getPid());
+                                  startActivity(intent);
+                      }
+                          });
+                      }
                      @NonNull
                      @Override
                      public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                         return null;
+
+                         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                         ProductViewHolder holder = new ProductViewHolder(view);
+                         return holder;
                      }
                  };
+
             }
 
-        }
+
