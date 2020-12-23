4adabb256acc6e34dbad806b901ceb52395bbf83
tranthaithinhhpq <55739883+tranthaithinhhpq@users.noreply.github.com>
2020-12-23 10:17:26 +0700

    xong 48

--- app/src/main/java/com/example/appcanifa/Model/Products.java
+++ app/src/main/java/com/example/appcanifa/Model/Products.java
@@ -1,12 +1,12 @@
 package com.example.appcanifa.Model;
 
 public class Products {
-    private String pname ,description, price, image, category, pid, date, time;
+    private String pname ,description, price, image, category, pid, date, time, productState;
     public Products(){
 
     }
 
-    public Products(String pname, String description, String price, String image, String category, String pid, String date, String time) {
+    public Products(String pname, String description, String price, String image, String category, String pid, String date, String time, String productState) {
         this.pname = pname;
         this.description = description;
         this.price = price;
@@ -15,6 +15,7 @@ public class Products {
         this.pid = pid;
         this.date = date;
         this.time = time;
+        this.productState = productState;
     }
 
     public String getPname() {
@@ -79,5 +80,13 @@ public class Products {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public String getProductState() {
+        return productState;
+    }
+
+    public void setProductState(String productState) {
+        this.productState = productState;
     }
 }
